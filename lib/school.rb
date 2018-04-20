aa5fdff35cea3f693fc0b8ab316e42fee74d07ac
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-04-20 22:00:31 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,6 +13,7 @@ class School
   def add_student(student_name,student_grade)
     @student_name = student_name
     @student_grade = student_grade
+    @roster[@student_name] = @student_grade
   end
 
   attr_reader :name
